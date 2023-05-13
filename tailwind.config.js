8c3d8783eadb9cdc71a28dfb2dd97a621df82512
ESS Systemes & Services <lyrbat@gmail.com>
2023-05-13 19:30:28 +0200

    Before video 29: restart lessons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      './layout/*.liquid',
-      './sections/*.liquid',
-      './snippets/*.liquid',
-      './templates/customers/*.liquid',
-      './templates/*.liquid'
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
-  
+  darkmode: false,
+  content: [
+    './layout/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templates/customers/*.liquid',
+    './templates/*.liquid'
+  ],
+  theme: {
+    extend: {
+      height: {
+          '94': '22rem',
+      },
+      spacing: {
+          '8xl': '96rem',
+          '9xl': '128rem'
+      }
+    }
+  },
+  plugins: [
+    // require('@tailwindcss/forms'),
+    // require('@tailwindcss/aspect-ratio'),
+    // require('@tailwindcss/typography'),
+    // require('tailwindcss-children'),
+  ],
+}
+
   
